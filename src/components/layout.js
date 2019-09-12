19b69c256cfd0eeec60f7cfee78f8423555e3394
Renal Apriansyah <renal.apr@gmail.com>
2019-09-12 22:23:55 +0700

    change layout to use drawer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,107 @@
-import React from "react"
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { css } from "@emotion/core"
 import { useStaticQuery, Link, graphql } from "gatsby"
+import NavBar from "./NavBar"
 import { rhythm } from "../utils/typography"
+import Grid from "@material-ui/core/Grid"
 
-export default ({ children }) => {
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+function Home(props) {
+  const { container } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  function handleDrawerToggle() {
+    setMobileOpen(!mobileOpen);
+  }
+
+  const LeftDrawer = ({ data }) => (
+    <div>
+      <div className={classes.toolbar}>
+        <Link to={`/`}>
+          <h3
+            css={css`
+              display: inline-block;
+              font-style: normal;
+            `}
+          >
+            { data.site.siteMetadata.title }
+          </h3>
+        </Link>
+      </div>
+      <Divider />
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+
 	const data = useStaticQuery(
     graphql`
       query {
@@ -15,35 +113,64 @@ export default ({ children }) => {
       }
     `
   )
-	return(
-  	<div
-  	  css={css`
-  	    margin: 0 auto;
-  	    max-width: 700px;
-  	    padding: ${rhythm(2)};
-  	    padding-top: ${rhythm(1.5)};
-  	  `}
-  	>
-  	  <Link to={`/`}>
-  	    <h3
-  	      css={css`
-  	        margin-bottom: ${rhythm(2)};
-  	        display: inline-block;
-  	        font-style: normal;
-  	      `}
-  	    >
-					{ data.site.siteMetadata.title }
-  	    </h3>
-  	  </Link>
-  	  <Link
-  	    to={`/about/`}
-  	    css={css`
-  	      float: right;
-  	    `}
-  	  >
-  	    About
-  	  </Link>
-  	  {children}
-  	</div>
-	)
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            <LeftDrawer data={data} />
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            <LeftDrawer data={data} />
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+          <div
+            css={css`
+              margin: 0 auto;
+              max-width: 700px;
+              padding: ${rhythm(2)};
+              padding-top: ${rhythm(1.5)};
+            `}
+          >
+            {props.children}
+          </div>
+      </main>
+    </div>
+  );
 }
+
+Home.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  container: PropTypes.instanceOf(typeof Element === 'undefined' ? Object : Element),
+};
+
+export default Home;
